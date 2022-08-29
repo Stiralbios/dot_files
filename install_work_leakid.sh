69554e53a15b1ddf0579be3880a7f998e0b82233
aurelien <aurelien.vezin@leakid.com>
2022-08-29 09:27:08 +0200

    fix aliases

--- install_work_leakid.sh
+++ install_work_leakid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp .bash_aliases ~/.bash_aliases
-cp .bash_aliases_leakid ~/.bash_aliases
+cp .bash_aliases_leakid ~/.bash_aliases_leakid
 cp .emacs ~/.emacs
 cp .gitconfig_leakid ~/.gitconfig
 cp .vimrc ~/.vimrc
