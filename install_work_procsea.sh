6340efa595399a0c2b37e33fdab18f236e2197da
Aurélien Vezin <a.vezin@procsea.com>
2022-12-12 13:56:10 +0100

    Change on procsea aliases

--- install_work_procsea.sh
+++ install_work_procsea.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp .bash_aliases ~/.bash_aliases
 cp .bash_aliases_procsea ~/.bash_aliases_procsea
+cp .bash_aliases_procsea_secret ~/.bash_aliases_procsea_secret
 cp .emacs ~/.emacs
 cp .gitconfig_procsea ~/.gitconfig
 cp .vimrc ~/.vimrc
