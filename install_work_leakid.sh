659d1d39ef9e93131739fd1b0bb55db5ca909643
Stiralbios <aurelien.vezin.dev@gmail.com>
2024-11-04 21:26:23 +0100

    remove old useless stuff

--- install_work_leakid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp .bash_aliases ~/.bash_aliases
-cp .bash_aliases_leakid ~/.bash_aliases_leakid
-cp .emacs ~/.emacs
-cp .gitconfig_leakid ~/.gitconfig
-cp .vimrc ~/.vimrc
-exec bash -l # load a new shell to get the differences
--- install_work_procsea.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp .bash_aliases ~/.bash_aliases
-cp .bash_aliases_procsea ~/.bash_aliases_procsea
-cp .bash_aliases_procsea_secret ~/.bash_aliases_procsea_secret
-cp .emacs ~/.emacs
-cp .gitconfig_procsea ~/.gitconfig
-cp .vimrc ~/.vimrc
-cp .inputrc ~/.inputrc
-exec bash -l # load a new shell to get the differences
